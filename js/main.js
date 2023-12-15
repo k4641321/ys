0b9cb2fa71455bca1652f79faefa2cafb4487528
k4641321 <74849935+k4641321@users.noreply.github.com>
2023-12-16 00:28:11 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function yfys(){
+    window.open('../zx.html','sfqd','width=400,height=100,menubar=no,toolbar=no,location=no,directories=no,status=no,scrollbars=yes,resizable=yes');
+}
+
+function show() {
+  var bg = document.getElementById("bg");
+  bg.style.display = "none";
+  var imgg = document.getElementById('imgm')
+  imgg.classList.add.('show');
+}
